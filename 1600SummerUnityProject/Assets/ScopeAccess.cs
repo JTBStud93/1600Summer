d90595b4308542ecbd60a87229b1f36850b36b9a
J. Tanner Breeze <JTBStud@gmail.com>
2017-06-19 10:57:27 -0600

    ScopeAccess

---
+++ 1600SummerUnityProject/Assets/ScopeAccess.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAccess : MonoBehaviour {
+
+	public int coins = 0;
+
+
+	void Attack(){
+
+		
+		void SwordAttack(){
+			if (SwordAttack > EnemyDefense){
+				EnemyHealth.addDamage();
+			}
+			else{
+				EndTurn();
+			}
+		}
+		
+		
+		void AxeAttack(){
+			if (AxeAttack > EnemyDefense){
+				EnemyHealth.addDamage();
+			}
+			else{
+				EndTurn();
+			}
+		}
+
+	}
+
+
+
+}
