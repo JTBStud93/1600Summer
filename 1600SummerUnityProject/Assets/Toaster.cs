618a41ff50c8d6d9945a868cf2ef6e049bdb6943
J. Tanner Breeze <JTBStud@gmail.com>
2017-06-21 11:15:01 -0600

    Void Functions

---
+++ 1600SummerUnityProject/Assets/Toaster.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toaster : MonoBehaviour {
+	public int num1;
+	public int num2;
+
+	void Start(){
+		int num = AddNumbers(num1, num2);
+		print(num);
+	}
+	
+	public int AddNumbers(int number1, int number2){
+		int result = number1 + number2;
+
+		return result;
+	}
+
+}
