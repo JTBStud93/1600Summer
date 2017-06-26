d530d8fbe52ee91743c92ded4cf92d2b264408bb
J. Tanner Breeze <JTBStud@gmail.com>
2017-06-26 11:32:59 -0600

    6/26/2017 Functions & basic calc

---
+++ 1600SummerUnityProject/Assets/basic_calc.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class basic_calc : MonoBehaviour {
+
+ public InputField in1;
+ public InputField in2;
+ public Text result;
+
+public void AddClick(){
+	int num = int.Parse(in1.text) + int.Parse(in2.text);
+	result.text = num.ToString();
+}
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
