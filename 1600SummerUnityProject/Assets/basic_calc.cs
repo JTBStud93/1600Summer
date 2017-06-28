04b78eab8442a78ec139a368bff96df1fb954f41
J. Tanner Breeze <JTBStud@gmail.com>
2017-06-27 19:18:58 -0600

    Included 3 other math operators (as comments).

--- 1600SummerUnityProject/Assets/basic_calc.cs
+++ 1600SummerUnityProject/Assets/basic_calc.cs
@@ -8,6 +8,16 @@ public class basic_calc : MonoBehaviour {
 
  public InputField in1;
  public InputField in2;
+	
+/*
+ public InputField in3;
+ public InputField in4;
+ public InputField in5;
+ public InputField in6;
+ public InputField in7;
+ public InputField in8;
+*/
+	
  public Text result;
 
 public void AddClick(){
@@ -15,7 +25,22 @@ public void AddClick(){
 	result.text = num.ToString();
 }
 
+/*
+public void SubtractClick(){
+	int num = int.Parse(in3.text) - int.Parse(in4.text);
+	result.text = num.ToString();
+}
 
+public void MultiplyClick(){
+	int num = int.Parse(in5.text) * int.Parse(in6.text);
+	result.text = num.ToString();
+}
+
+public void DivideClick(){
+	int num = int.Parse(in7.text) / int.Parse(in8.text);
+	result.text = num.ToString();
+}
+*/
 
 
 
