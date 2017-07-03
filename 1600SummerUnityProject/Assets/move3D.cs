4822dae0114f00b9682b1ee62f516010fa80379e
J. Tanner Breeze <JTBStud@gmail.com>
2017-07-03 11:20:57 -0600

    funfunfun
    Making a cube move!

---
+++ 1600SummerUnityProject/Assets/move3D.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move3D : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed;
+
+	void Update() {
+		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+
+		transform.Rotate(0,y,0);
+		transform.Translate(0,0,z);
+	}
+}
