770d7cec765f7c233c7ee136097e0bd340967773
J. Tanner Breeze <JTBStud@gmail.com>
2017-07-17 11:19:34 -0600

    Score Manager

--- 1600SummerUnityProject/Assets/Scripts/Destroy.cs
+++ 1600SummerUnityProject/Assets/Scripts/Destroy.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class Destroy : MonoBehaviour {
 	public Transform spawnPoint;
+	public int points;
 
 	void OnCollisionEnter(Collision other)
 	{
 		if(other.gameObject.name == "Player"){
 			//Destroy(gameObject);
+			scoreManager.AddPoints(points);
 			transform.position = spawnPoint.position;
 			transform.rotation = spawnPoint.rotation;
 		}
