7fa4663ef328564c781f02877e837839f1df6710
J. Tanner Breeze <JTBStud@gmail.com>
2017-07-26 11:30:06 -0600

    Coroutine & Shooting

--- 1600SummerUnityProject/Assets/Scripts/CoroutineScript.cs
+++ 1600SummerUnityProject/Assets/Scripts/CoroutineScript.cs
@@ -1,24 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class move3D : MonoBehaviour {
-	public float moveSpeed;
-	public float turnSpeed;
-	public float jumpHeight;
+public class CoroutineScript : MonoBehaviour {
 
-
-
-	void Update() {
-		var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
-		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
-		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
-
-		transform.Rotate(0,y,0);
-		transform.Translate(0,0,z);
-		transform.Translate(0,j,0);
-	}
-
-	/*
 	public float smoothing = 1f;
 	public Transform target;
 	
@@ -43,5 +27,4 @@ public class move3D : MonoBehaviour {
 		print("MyCoroutine is now finished.");
 	}
 
-	*/
 }
